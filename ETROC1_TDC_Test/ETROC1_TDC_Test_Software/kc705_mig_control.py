36068bbef61584b229d1a5cb381fcf176a0410b2
Zhang <weizhang@smu.edu>
2019-11-27 13:09:12 -0600

    add ETROC1_TDCReg class

--- ETROC1_TDC_Test/ETROC1_TDC_Test_Software/kc705_mig_control.py
+++ ETROC1_TDC_Test/ETROC1_TDC_Test_Software/kc705_mig_control.py
@@ -115,25 +115,63 @@ def data_plot(data):
     plt.savefig("DDR3_Fetched_data.png", dpi=fig_dpi, bbox_inches='tight')         # save figure
     plt.clf()
 #--------------------------------------------------------------------------#
-## main function
+## Manage ETROC1 TDC chip's internal registers map
+# Allow combining and disassembling individual registers
+class ETROC1_TDCReg(object):
+    ## @var _defaultRegMap default register values
+    _defaultRegMap = {
+        'Dataout_disCMLDriver_BIAS'     :   0,
+        'Clk40Mout_disCMLDriver_BIAS'   :   0,
+        'tdc_offset'                    :   0x0,
+        'tdc_enable'                    :   1
+    }
+    ## @var register map local to the class
+    _regMap = {}
+
+    def __init__(self):
+        self._regMap = copy.deepcopy(self._defaultRegMap)
+
+    def set_Dataout_disCMLDriver_BIAS(self, ONOFF):                             # ONOFF = 0, CML driver bias enable, ONOFF = 1 CML driver bias disable
+        self._regMap['Dataout_disCMLDriver_BIAS'] = 0x1 & ONOFF
+
+    def set_Clk40Mout_disCMLDriver_BIAS(self, ONOFF):
+        self._regMap['Clk40Mout_disCMLDriver_BIAS'] = 0x1 & ONOFF               # ONOFF = 0, CML driver bias enable, ONOFF = 1 CML driver bias disable
+
+    def set_tdc_offset(self, val):
+        self._regMap['tdc_offset'] = 0x7f & val                                 # measurement window offset 0-127
+
+    def set_tdc_enable(self, ONOFF):
+        self._regMap['tdc_enable'] = 0x1 & ONOFF                                # ONOFF = 0, disable TDC controller, ONOFF = 1 enable TDC controller
+
+    ## get I2C register value
+    def get_config_vector(self):
+        reg_value = []
+        reg_value += [self._regMap['Clk40Mout_disCMLDriver_BIAS'] << 1 | self._regMap['Dataout_disCMLDriver_BIAS']]             # register 0x00
+        reg_value += [self._regMap['tdc_enable'] << 7 | self._regMap['tdc_offset']]                                             # register 0x01
+        return reg_value
+#--------------------------------------------------------------------------#
+## main functionl
 def main():
-    reg_addr = [0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d,\
-                0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b]
-    for j in range(len(reg_addr)):
-        iic_write(1, 0x22, 0, reg_addr[j], j)
-    print(len(reg_addr))
-    for i in range(len(reg_addr)):
-        print("reg addr %s:"%str(hex(reg_addr[i])), hex(iic_read(0, 0x22, 1, reg_addr[i])))
+    # reg_addr = [0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d,\
+    #             0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1a, 0x1b]
+    # for j in range(len(reg_addr)):
+    #     iic_write(1, 0x22, 0, reg_addr[j], j)
+    # print(len(reg_addr))
+    # for i in range(len(reg_addr)):
+    #     print("reg addr %s:"%str(hex(reg_addr[i])), hex(iic_read(0, 0x22, 1, reg_addr[i])))
     # data_out = []
     # data_out = test_ddr3()
     # print(data_out)
     # data_plot(data_out)
+    etroc1_tdcReg = ETROC1_TDCReg()
+    etroc1_tdcReg.set_tdc_offset(0x01)
+    print(etroc1_tdcReg.get_config_vector())
     print("Ok!")
 #--------------------------------------------------------------------------#
 ## if statement
 if __name__ == "__main__":
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)	#initial socket
-	s.connect((hostname, port))								#connect socket
-	cmd_interpret = command_interpret(s)					#Class instance
+	# s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)	#initial socket
+	# s.connect((hostname, port))								#connect socket
+	# cmd_interpret = command_interpret(s)					#Class instance
 	main()													#execute main function
-	s.close()												#close socket
+	# s.close()												#close socket
