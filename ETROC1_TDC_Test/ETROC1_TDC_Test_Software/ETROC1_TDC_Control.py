d98a1c7aef3dfedb1a6d2e2df49dad2dc56823ea
Zhang <weizhang@smu.edu>
2020-08-07 15:15:40 -0500

    fixed DDR2 python script bug

--- ETROC1_TDC_Test/ETROC1_TDC_Test_Software/ETROC1_TDC_Control.py
+++ ETROC1_TDC_Test/ETROC1_TDC_Test_Software/ETROC1_TDC_Control.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+import os
 import sys
 import copy
 import time
@@ -7,6 +8,7 @@ import visa
 import datetime
 import struct
 import socket
+import winsound
 import heartrate
 from command_interpret import *
 from ETROC1_TDCReg import *
@@ -48,19 +50,19 @@ def test_ddr3(data_num):
     cmd_interpret.write_config_reg(0, 0x0000)       # written disable
     cmd_interpret.write_pulse_reg(0x0040)           # reset ddr3 control logic
     time.sleep(0.01)
-    # print("sent pulse!")
+    print("sent pulse!")
 
-    write_data_into_ddr3(1, 0x0000000, 0x6000000)   # set write begin address and post trigger address and wrap around
+    write_data_into_ddr3(1, 0x0000000, 0x7000000)   # set write begin address and post trigger address and wrap around
     cmd_interpret.write_pulse_reg(0x0008)           # writing start
     time.sleep(0.1)
     cmd_interpret.write_config_reg(0, 0x0001)       # written enable fifo32to256
-    time.sleep(0.01)
+    time.sleep(0.1)
     cmd_interpret.write_pulse_reg(0x0010)           # writing stop
 
-    time.sleep(2)
+    time.sleep(4)
     cmd_interpret.write_config_reg(0, 0x0000)       # write enable fifo32to256
     time.sleep(4)
-    read_data_from_ddr3(0x6000000)                  # set read begin address
+    read_data_from_ddr3(0x7000000)                  # set read begin address
 
     data_out = []
     ## memoryview usage
@@ -113,7 +115,7 @@ def main():
 
     slave_addr = 0x23                                       # I2C slave address
 
-    userdefinedir = "Jitter_Performance_P%d_QInj=1M25_V1"
+    userdefinedir = "TDC_Random_Occupancy=5%_PulseStrobe_0x03"
 
     ##  Creat a directory named path with date of today
     today = datetime.date.today()
@@ -131,9 +133,10 @@ def main():
 
     ## setting parameters
     Pulse_Strobe = 0x03                 # 0x03: 3.125 ns Cal Code
+    Board_Num = 1                       # Board Number
     testMode = 0                        # 0: nromal mode 1: test mode
     polaritySel = 1                     # 0: high power mode, 1: low power mode
-    Total_point = 4                     # total fetch data = Total_point * 50000
+    Total_point = 400                   # total fetch data = Total_point * 50000
     fetch_data = 1
 
     reg_val = []
@@ -161,7 +164,7 @@ def main():
 
     ## DMRO setting
     ETROC1_TDCReg1.set_DMRO_testMode(0)
-    ETROC1_TDCReg1.set_DMRO_enable(0)           ## enable Scrambler
+    ETROC1_TDCReg1.set_DMRO_enable(1)           ## enable Scrambler
     Enable_FPGA_Descrablber(1)                  ## Enable FPGA Firmware Descrambler
 
     ETROC1_TDCReg1.set_DMRO_resetn(1)
@@ -212,28 +215,23 @@ def main():
 
     if fetch_data == 1:                 # fetch data switch
         time_stamp = time.strftime('%m-%d_%H-%M-%S',time.localtime(time.time()))
-        filename = "TDC_Data_TestMode=%d_polaritySel=%d_Pulse_Strobe=%s_%s_%s"%(testMode, polaritySel, Pulse_Strobe, Total_point*50000, time_stamp)
-        with open(filename, 'w') as infile:
-            for k in range(1):
-                # print("Fetching NO.%01d file..."%k)
-                data_out = [0]
-                data_out = test_ddr3(Total_point)      # num: The total fetch data num * 50000
-                TOA_Code = []
-                TOT_Code = []
-                Cal_Code = []
-                for i in range(len(data_out)):
-                    TDC_data = []
-                    for j in range(30):
-                        TDC_data += [((data_out[i] >> j) & 0x1)]
-                    hitFlag = TDC_data[29]
-                    TOT_Code1 = TDC_data[0] << 8 | TDC_data[1] << 7 | TDC_data[2] << 6 | TDC_data[3] << 5 | TDC_data[4] << 4 | TDC_data[5] << 3 | TDC_data[6] << 2 | TDC_data[7] << 1 | TDC_data[8]
-                    TOA_Code1 = TDC_data[9] << 9 | TDC_data[10] << 8 | TDC_data[11] << 7 | TDC_data[12] << 6 | TDC_data[13] << 5 | TDC_data[14] << 4 | TDC_data[15] << 3 | TDC_data[16] << 2 | TDC_data[17] << 1 | TDC_data[18]
-                    Cal_Code1 = TDC_data[19] << 9 | TDC_data[20] << 8 | TDC_data[21] << 7 | TDC_data[22] << 6 | TDC_data[23] << 5 | TDC_data[24] << 4 | TDC_data[25] << 3 | TDC_data[26] << 2 | TDC_data[27] << 1 | TDC_data[28]
-                    TOA_Code += [TOA_Code1]
-                    TOT_Code += [TOT_Code1]
-                    Cal_Code += [Cal_Code1]
+        filename = "TDC_Data_Random_Occupancy=5_TestMode=%d_polaritySel=%d_PulseStrobe=%s_B%d_%s_%s.dat"%(testMode, polaritySel, hex(Pulse_Strobe), Board_Num, Total_point*50000, time_stamp)
+        print(filename)
+        with open("./%s/%s/%s"%(todaystr, userdefinedir, filename), 'w') as infile:
 
-                    infile.write("%3d %3d %3d %d\n"%(TOA_Code1, TOT_Code1, Cal_Code1, hitFlag))
+            data_out = [0]
+            data_out = test_ddr3(Total_point)      # num: The total fetch data num * 50000
+            print("Start store data......")
+
+            for i in range(len(data_out)):
+                TDC_data = []
+                for j in range(30):
+                    TDC_data += [((data_out[i] >> j) & 0x1)]
+                hitFlag = TDC_data[29]
+                TOT_Code1 = TDC_data[0] << 8 | TDC_data[1] << 7 | TDC_data[2] << 6 | TDC_data[3] << 5 | TDC_data[4] << 4 | TDC_data[5] << 3 | TDC_data[6] << 2 | TDC_data[7] << 1 | TDC_data[8]
+                TOA_Code1 = TDC_data[9] << 9 | TDC_data[10] << 8 | TDC_data[11] << 7 | TDC_data[12] << 6 | TDC_data[13] << 5 | TDC_data[14] << 4 | TDC_data[15] << 3 | TDC_data[16] << 2 | TDC_data[17] << 1 | TDC_data[18]
+                Cal_Code1 = TDC_data[19] << 9 | TDC_data[20] << 8 | TDC_data[21] << 7 | TDC_data[22] << 6 | TDC_data[23] << 5 | TDC_data[24] << 4 | TDC_data[25] << 3 | TDC_data[26] << 2 | TDC_data[27] << 1 | TDC_data[28]
+                infile.write("%3d %3d %3d %d\n"%(TOA_Code1, TOT_Code1, Cal_Code1, hitFlag))
 
 if __name__ == "__main__":
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)	#initial socket
